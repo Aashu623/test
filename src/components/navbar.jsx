c3f8c2e6f34ee26867ab98110b92d099a2156907
Aashu623 <aashukushwah53678@gmail.com>
2024-08-07 12:25:19 +0530

    Revert "added navlist"
    This reverts commit 271bf5632ed2f22e016969d2441741dfb87fd274.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,20 +4,6 @@ function navbar() {
   return (
     <div>
       <img src="logo.png" alt="" />
-      <ul>
-        <li>
-          <a href="">Home</a>
-        </li>
-        <li>
-          <a href="">About</a>
-        </li>
-        <li>
-          <a href="">Services</a>
-        </li>
-        <li>
-          <a href="">Contact</a>
-        </li>
-      </ul>
     </div>
   );
 }
