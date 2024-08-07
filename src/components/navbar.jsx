92757ff0f685eb8ccb61e97e3402cd5089a59383
Aashu623 <aashukushwah53678@gmail.com>
2024-08-07 12:20:36 +0530

    added logo in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function navbar() {
-  return <div>navbar</div>;
+  return (
+    <div>
+      <img src="logo.png" alt="" />
+    </div>
+  );
 }
 
 export default navbar;
