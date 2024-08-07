5ae9973a8fcea55d487cf0c93344c622f11b7f55
Aashu623 <aashukushwah53678@gmail.com>
2024-08-07 14:08:56 +0530

    add simple about us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,13 @@
 import "./App.css";
-import { useState } from "react";
+import Header from "./components/Header";
+import About from "./components/About";
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [age, setAge] = useState("");
-  const [gender, setGender] = useState("");
-  const [phone, setPhone] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const myForm = new FormData();
-    myForm.set("firstName", firstName);
-    myForm.set("lastName", lastName);
-    myForm.set("age", age);
-    myForm.set("gender", gender);
-    myForm.set("phone", phone);
-  };
-
   return (
-    <div>
-      <h1>User Details Form</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>First Name</label>
-          <input
-            type="text"
-            name="firstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Last Name</label>
-          <input
-            type="text"
-            name="lastName"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Age</label>
-          <input
-            type="text"
-            name="age"
-            value={age}
-            onChange={(e) => setAge(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Male</label>
-          <input
-            type="radio"
-            name="gender"
-            value="Male"
-            onChange={(e) => setGender(e.target.value)}
-          />
-          <label>Female</label>
-          <input
-            type="radio"
-            name="gender"
-            value="Female"
-            onChange={(e) => setGender(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Phone</label>
-          <input
-            type="text"
-            name="phone"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
-        </div>
-        <input type="submit" />
-      </form>
-    </div>
+    <>
+      <Header />
+      <About />
+    </>
   );
 }
 
