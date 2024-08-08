309f71a130b85122eb099a4a0bb3449fb5d845e0
Aashu623 <aashukushwah53678@gmail.com>
2024-08-08 17:15:39 +0530

    install openmeteo packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import "./App.css";
+import { fetchWeatherApi } from "openmeteo";
 
 function App() {
-  const btn = useRef();
-  const [count, setCount] = useState(0);
-
   useEffect(() => {
-    const handleClick = () => {
-      setCount((count) => count + 1);
-    };
-
-    const buttonElement = btn.current;
-
-    if (buttonElement) {
-      buttonElement.addEventListener("click", handleClick);
+    async function fetchWeather() {
+      const params = {
+        latitude: [22.7179],
+        longitude: [75.8333],
+        hourly: "temperature_2m",
+      };
+      const url = "https://api.open-meteo.com/v1/forecast";
+      const responses = await fetchWeatherApi(url, params);
+      const response = responses[0];
+      console.log(response);
+      const utcOffsetSeconds = response.utcOffsetSeconds();
+      const timezone = response.timezone();
+      const timezoneAbbreviation = response.timezoneAbbreviation();
+      const latitude = response.latitude();
+      const longitude = response.longitude();
+      console.log(
+        utcOffsetSeconds,
+        timezone,
+        timezoneAbbreviation,
+        latitude,
+        longitude
+      );
     }
-
-    return () => {
-      if (buttonElement) {
-        buttonElement.removeEventListener("click", handleClick);
-      }
-    };
+    fetchWeather();
   });
   return (
     <div className="flex w-screen h-screen">
       <div className="container flex flex-col w-2/6 bg-red-100 p-10 gap-5 items-center self-center">
-        <span className="text-2xl">Button click count: {count}</span>
-        <button
-          className="text-2xl border p-2 border-black  rounded-sm"
-          ref={btn}
-        >
+        <span className="text-2xl">Button click count:</span>
+        <button className="text-2xl border p-2 border-black  rounded-sm">
           Click me
         </button>
       </div>
