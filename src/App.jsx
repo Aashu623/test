ea921e5da399631ebdfd4be34494808e88c99740
Aashu623 <aashukushwah53678@gmail.com>
2024-08-08 12:09:12 +0530

    add button using useRef Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import Product from "./components/Product";
-import TodoList from "./components/TodoList";
 
 function App() {
+  const btn = useRef();
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const handleClick = () => {
+      setCount((count) => count + 1);
+    };
+
+    const buttonElement = btn.current;
+
+    if (buttonElement) {
+      buttonElement.addEventListener("click", handleClick);
+    }
+
+    return () => {
+      if (buttonElement) {
+        buttonElement.removeEventListener("click", handleClick);
+      }
+    };
+  });
   return (
-    <>
-      <TodoList />
-      <Product />
-    </>
+    <div className="flex w-screen h-screen">
+      <div className="container flex flex-col w-2/6 bg-red-100 p-10 gap-5 items-center self-center">
+        <span className="text-2xl">Button click count: {count}</span>
+        <button
+          className="text-2xl border p-2 border-black  rounded-sm"
+          ref={btn}
+        >
+          Click me
+        </button>
+      </div>
+    </div>
   );
 }
 
